e233821dd00f1d47d326c4b067bd011b2c7442dc
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-06-08 16:18:54 -0300

    Realizei requisitos 3 e 4, onde criei a lógica das filtragens

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,44 +1,76 @@
 import React, { useContext } from 'react';
 import TableContext from '../context/TableContext';
 
+const TABLEHEADER = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter',
+  'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population',
+  'Films', 'Created', 'Edited', 'URL'];
+
 function Table() {
-  const { nameFiltred, handleChange } = useContext(TableContext);
-  // console.log(nameFiltred);
-  const tableHeader = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter',
-    'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population',
-    'Films', 'Created', 'Edited', 'URL'];
+  const { apiFiltered, handleChangeName, numberFiltred,
+    handleFilters, handleChangeNumericValues, dropdow } = useContext(TableContext);
+
   return (
     <main>
       <form>
         <label htmlFor="name">
-          <input data-testid="name-filter" id="name" onChange={ handleChange } />
+          <input data-testid="name-filter" id="name" onChange={ handleChangeName } />
         </label>
+        <select
+          data-testid="column-filter"
+          name="colum"
+          onChange={ handleChangeNumericValues }
+        >
+          {dropdow.map((sl) => <option key={ sl }>{sl}</option>)}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="operator"
+          onChange={ handleChangeNumericValues }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          type="number"
+          value={ numberFiltred }
+          data-testid="value-filter"
+          name="number"
+          onChange={ handleChangeNumericValues }
+        />
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ handleFilters }
+        >
+          Filtrar
+
+        </button>
       </form>
       <table>
         <thead>
           <tr>
-            {tableHeader.map((title) => <th key={ title }>{title}</th>)}
+            {TABLEHEADER.map((title) => <th key={ title }>{title}</th>)}
           </tr>
         </thead>
         <tbody>
-          {nameFiltred
-        && nameFiltred.map((plan) => (
-          <tr key={ plan.name }>
-            <td>{plan.name}</td>
-            <td>{plan.rotation_period}</td>
-            <td>{plan.orbital_period}</td>
-            <td>{plan.diameter}</td>
-            <td>{plan.climate}</td>
-            <td>{plan.gravity}</td>
-            <td>{plan.terrain}</td>
-            <td>{plan.surface_water}</td>
-            <td>{plan.population}</td>
-            <td>{plan.films}</td>
-            <td>{plan.created}</td>
-            <td>{plan.edited}</td>
-            <td>{plan.url}</td>
-          </tr>
-        ))}
+          {apiFiltered.map((plan) => (
+            <tr key={ plan.name }>
+              <td>{plan.name}</td>
+              <td>{plan.rotation_period}</td>
+              <td>{plan.orbital_period}</td>
+              <td>{plan.diameter}</td>
+              <td>{plan.climate}</td>
+              <td>{plan.gravity}</td>
+              <td>{plan.terrain}</td>
+              <td>{plan.surface_water}</td>
+              <td>{plan.population}</td>
+              <td>{plan.films}</td>
+              <td>{plan.created}</td>
+              <td>{plan.edited}</td>
+              <td>{plan.url}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
--- src/context/TableProvider.jsx
+++ src/context/TableProvider.jsx
@@ -3,32 +3,87 @@ import propTypes from 'prop-types';
 import TableContext from './TableContext';
 
 function TableProvider({ children }) {
-  const [planetsList, setPlanetsList] = useState([]);
-  const [name, setName] = useState('');
-  const [nameFiltred, setNameFiltred] = useState([]);
+  const [returnApi, setReturnApi] = useState([]);
+  const [planetsName, setPlanetsName] = useState('');
+  const [apiFiltered, setApiFiltered] = useState([]);
+  const [columFiltred, setColumFiltred] = useState('population');
+  const [operatorFiltred, setOperatorFiltred] = useState('maior que');
+  const [numberFiltred, setNumberFiltred] = useState(0);
+  const [allFilters, setAllFilters] = useState([]);
+  const [dropdow, setDropdow] = useState(['population', 'orbital_period',
+    'diameter', 'rotation_period', 'surface_water']);
 
   const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
   useEffect(() => {
     const getPlanets = async () => {
       const { results } = await fetch(ENDPOINT).then((response) => response.json());
-      setPlanetsList(results);
-      setNameFiltred(results);
+      setReturnApi(results);
+      setApiFiltered(results);
     };
     getPlanets();
   }, []);
 
-  const handleChange = ({ target }) => {
-    setName(target.value);
+  const handleChangeName = ({ target }) => {
+    setPlanetsName(target.value);
+  };
+
+  const updateDropdow = () => {
+    const dropdowFiltered = dropdow.filter((drop) => drop !== columFiltred);
+    setDropdow(dropdowFiltered);
   };
 
   useEffect(() => {
-    const filteredName = planetsList
-      .filter((plan) => plan.name.toLowerCase().includes(name));
-    setNameFiltred(filteredName);
-  }, [name, planetsList]);
+    const filteredName = returnApi
+      .filter((plan) => plan.name.toLowerCase().includes(planetsName));
+
+    const resultFilter = allFilters.reduce((acumulator, filter) => acumulator
+      .filter((planet) => {
+        switch (filter.operatorFiltred) {
+        case 'maior que':
+          return planet[filter.columFiltred] > Number(filter.numberFiltred);
+        case 'menor que':
+          return planet[filter.columFiltred] < Number(filter.numberFiltred);
+        case 'igual a':
+          return Number(planet[filter.columFiltred]) === Number(filter.numberFiltred);
+        default:
+          return true;
+        }
+      }), filteredName);
+    setApiFiltered(resultFilter);
+  }, [planetsName, allFilters, returnApi]);
+
+  const handleChangeNumericValues = ({ target }) => {
+    const { name, value } = target;
+    if (name === 'colum') {
+      setColumFiltred(value);
+    } else if (name === 'operator') {
+      setOperatorFiltred(value);
+    } else {
+      setNumberFiltred(value);
+    }
+  };
+
+  const handleFilters = () => {
+    const filters = {
+      columFiltred,
+      operatorFiltred,
+      numberFiltred,
+    };
+    setAllFilters([...allFilters, filters]);
+    updateDropdow();
+  };
+
+  const props = {
+    apiFiltered,
+    numberFiltred,
+    dropdow,
+    handleChangeName,
+    handleFilters,
+    handleChangeNumericValues,
+  };
 
   return (
-    <TableContext.Provider value={ { nameFiltred, handleChange } }>
+    <TableContext.Provider value={ props }>
       { children }
     </TableContext.Provider>
   );
