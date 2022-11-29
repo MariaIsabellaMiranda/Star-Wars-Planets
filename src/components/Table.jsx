6b5b5f72ccb40cbe417da40738d08fe3df6d10b7
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-29 15:55:34 -0300

    refactor: [Table] Retirados Filtros para uma melhor organização do component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,102 +1,43 @@
 import React, { useContext } from 'react';
 import TableContext from '../context/TableContext';
+import '../Style/Table.css';
 
 const TABLEHEADER = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter',
   'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population',
   'Films', 'Created', 'Edited', 'URL'];
 
 function Table() {
-  const { apiFiltered, handleChangeName, numberFiltred,
-    handleFilters, handleChangeNumericValues, dropdow,
-    allFilters, deleteFilters } = useContext(TableContext);
+  const {
+    apiFiltered,
+  } = useContext(TableContext);
 
   return (
-    <main>
-      <form>
-        <label htmlFor="name">
-          <input data-testid="name-filter" id="name" onChange={ handleChangeName } />
-        </label>
-        <select
-          data-testid="column-filter"
-          name="colum"
-          onChange={ handleChangeNumericValues }
-        >
-          {dropdow.map((sl) => <option key={ sl }>{sl}</option>)}
-        </select>
-        <select
-          data-testid="comparison-filter"
-          name="operator"
-          onChange={ handleChangeNumericValues }
-        >
-          <option>maior que</option>
-          <option>menor que</option>
-          <option>igual a</option>
-        </select>
-        <input
-          type="number"
-          value={ numberFiltred }
-          data-testid="value-filter"
-          name="number"
-          onChange={ handleChangeNumericValues }
-        />
-        <button
-          data-testid="button-filter"
-          type="button"
-          onClick={ handleFilters }
-        >
-          Filtrar
-
-        </button>
-        <button
-          type="button"
-          data-testid="button-remove-filters"
-          onClick={ () => deleteFilters(allFilters) }
-        >
-          Remover Filtros
-
-        </button>
-      </form>
-      {allFilters.map((filter, index) => (
-        <div key={ index } data-testid="filter">
-          <p>
-            {`${filter.columFiltred}${filter.operatorFiltred}${filter.numberFiltred}`}
-          </p>
-          <button
-            type="button"
-            onClick={ () => deleteFilters(filter.columFiltred) }
-          >
-            Excluir
-
-          </button>
-        </div>
-      ))}
-      <table>
-        <thead>
-          <tr>
-            {TABLEHEADER.map((title) => <th key={ title }>{title}</th>)}
+    <table>
+      <thead>
+        <tr>
+          {TABLEHEADER.map((title) => <th key={ title }>{title}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {apiFiltered.map((plan) => (
+          <tr key={ plan.name }>
+            <td data-testid="planet-name">{plan.name}</td>
+            <td>{plan.rotation_period}</td>
+            <td>{plan.orbital_period}</td>
+            <td>{plan.diameter}</td>
+            <td>{plan.climate}</td>
+            <td>{plan.gravity}</td>
+            <td>{plan.terrain}</td>
+            <td>{plan.surface_water}</td>
+            <td>{plan.population}</td>
+            <td>{plan.films}</td>
+            <td>{plan.created}</td>
+            <td>{plan.edited}</td>
+            <td>{plan.url}</td>
           </tr>
-        </thead>
-        <tbody>
-          {apiFiltered.map((plan) => (
-            <tr key={ plan.name }>
-              <td>{plan.name}</td>
-              <td>{plan.rotation_period}</td>
-              <td>{plan.orbital_period}</td>
-              <td>{plan.diameter}</td>
-              <td>{plan.climate}</td>
-              <td>{plan.gravity}</td>
-              <td>{plan.terrain}</td>
-              <td>{plan.surface_water}</td>
-              <td>{plan.population}</td>
-              <td>{plan.films}</td>
-              <td>{plan.created}</td>
-              <td>{plan.edited}</td>
-              <td>{plan.url}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
